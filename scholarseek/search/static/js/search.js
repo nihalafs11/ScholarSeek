5ec6e298a139eccc46c7fa84a864b076de797243
Nihal Afsal <nihalafs11@users.noreply.github.com>
2023-12-07 18:07:41 -0500

    Delete scholarseek/search/static/js directory

--- scholarseek/search/static/js/search.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('optionsButton').addEventListener('click', function() {
-    var options = document.getElementById('optionsCheckbox');
-    if (options.style.display === 'none') {
-        options.style.display = 'block';
-    } else {
-        options.style.display = 'none';
-    }
-});
-
